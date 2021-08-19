d5a1a0b04e2c5e5292e217063d744b4f85d7df65
3009aksingh <3009aksingh@gmail.com>
2021-08-19 08:28:00 +0530

    commit

---
+++ 1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int m = 10, n = 20;
+    printf("%d %d %d", m /* m-value*/, /*n-value*/ n, m /*compute m*n*/ * n);
+    return (0);
+}
---
+++ 2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("%d ", printf("Hi!") * printf("Hey"));
+    return (0);
+}
